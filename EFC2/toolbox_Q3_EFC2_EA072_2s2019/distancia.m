683c56598d54e82762e7eebe808a38a40877ec39
patrickctrf <patrickctrf@gmail.com>
2019-11-29 06:13:01 -0200

    Acabou a disciplina.

--- EFC2/toolbox_Q3_EFC2_EA072_2s2019/distancia.m
+++
@@ -1,6 +0,0 @@
-%rotina que calcula a distancia euclidiana entre dois indivíduos
-
-function dist = distancia(aux, aux1)
-
-%distancia euclidiana entre os indivíduos
-dist = sqrt(sum((aux-aux1).^2));
